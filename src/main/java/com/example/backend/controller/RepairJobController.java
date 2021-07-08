9552513198da794d07bb6ef824a5b52da3ac0db8
D-vanheesch <Davidvanheesch@gmail.com>
2021-07-08 14:17:03 +0200

    Made model RepairJob.

---
+++ src/main/java/com/example/backend/controller/RepairJobController.java
@@ -0,0 +1,46 @@
+package com.example.backend.controller;
+
+import com.example.backend.model.RepairJob;
+import com.example.backend.service.RepairJobService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/repair-jobs")
+public class RepairJobController {
+
+    @Autowired
+    private RepairJobService repairJobService;
+
+        //krijgt verzoek binnen
+        @GetMapping("")
+        public ResponseEntity<Object> getRepairJob() {
+            return ResponseEntity.ok(repairJobService.getRepairJob());
+        }
+        //create employee rest API
+        @PostMapping("")
+        public ResponseEntity<Object> addRepairJob(@RequestBody RepairJob repairJob) {
+            repairJobService.addRepairJob(repairJob);
+            return ResponseEntity.ok("Added");
+        }
+        //get 1 employee
+        //krijgt verzoek binnen
+        @GetMapping("/{id}")
+        public ResponseEntity<Object> getRepairJob(@PathVariable("id") long id) {
+            RepairJob repairJob = repairJobService.getRepairJob(id);
+            return ResponseEntity.ok(repairJob);
+        }
+        //query to search on firstname (customers/firstname?firstname=David
+        @PutMapping("/{id}")
+        public ResponseEntity<Object> updateRepairJob(@PathVariable("id") long id, @RequestBody RepairJob updateRepairJob) {
+            repairJobService.updateRepairJob(id, updateRepairJob);
+            return ResponseEntity.noContent().build();
+        }
+        @DeleteMapping("/{id}")
+        public ResponseEntity<Object> removeRepairJob (@PathVariable("id") long id) {
+            repairJobService.removeRepairJob(id);
+            return ResponseEntity.noContent().build().ok("Deleted");
+        }
+
+}
