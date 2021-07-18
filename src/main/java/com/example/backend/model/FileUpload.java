715accafdbc5ca29d05bc454ffadd10373a74d4e
D-vanheesch <Davidvanheesch@gmail.com>
2021-07-18 14:22:59 +0200

    added fileLocation

--- src/main/java/com/example/backend/model/FileUpload.java
+++ src/main/java/com/example/backend/model/FileUpload.java
@@ -7,46 +7,61 @@ import javax.persistence.*;
 @Table
 public class FileUpload {
 
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
-
     private String fileName;
-
     private String title;
-
     private String description;
-
     private String base64;
+    private String fileLocation;
+
+
     public long getId() {
         return id;
     }
+
     public void setId(long id) {
         this.id = id;
     }
+
     public String getFileName() {
         return fileName;
     }
+
     public void setFileName(String fileName) {
         this.fileName = fileName;
     }
+
     public String getTitle() {
         return title;
     }
+
     public void setTitle(String title) {
         this.title = title;
     }
+
     public String getDescription() {
         return description;
     }
+
     public void setDescription(String description) {
         this.description = description;
     }
+
     public String getBase64() {
         return base64;
     }
+
     public void setBase64(String base64) {
         this.base64 = base64;
+    }
+
+    public String getFileLocation() {
+        return fileLocation;
+    }
+
+    public void setFileLocation(String fileLocation) {
+        this.fileLocation = fileLocation;
     }
 }
